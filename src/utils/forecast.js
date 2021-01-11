31cd537f91d9c28145f330c26d2f5bfafbe2f678
AbhishekKumar102K <f20180653@hyderabad.bits-pilani.ac.in>
2021-01-11 19:39:05 +0530

    Changed forecast info

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -3,14 +3,14 @@ const request = require('request')
 const forecast = (lat, lon, callback) => {
 
     const url = 'http://api.weatherstack.com/current?access_key=cb6520c20b314d813d06958288958f5b&query=' + lat + ',' + lon
-    request({ url, json: true }, (error,{body}) => {
+    request({ url, json: true }, (error,{body}={}) => {
         // console.log(response.body.current)
         if(error)
             callback("Unable to connect to the weather service", undefined)
         else if (body.error)
             callback("Unable to find location",undefined)    
         else
-            callback(undefined, "It is currently " + body.current.temperature + " degrees out. It feels like "+body.current.feelslike)
+            callback(undefined, "It is currently " + body.current.temperature + " degrees out. It feels like "+body.current.feelslike+". The wind speed is "+body.current.wind_speed+" kmph")
     })
 }
 
